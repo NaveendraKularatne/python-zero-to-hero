48bffdff89bb2152b763b46159e4187b8ab6fff7
NRK <naveendrarashmika@gmail.com>
2021-05-24 23:08:54 +0530

    How to get to know the dimensions of the image and then resizing & Cropping the image

--- 3_ResizingAndCropping.py
+++ 3_ResizingAndCropping.py
@@ -1,11 +1,13 @@
 import cv2
 
 img = cv2.imread("Resources/motor.jpg")
-ResizedImage = cv2.resize(img, (300, 257))
+ResizedImage = cv2.resize(img, (300, 257))      # Resizing the image
+CroppedImage = img[10:300, 250:450]             # Cropping the image
 
-print(img.shape)
+print(img.shape)                # checking the dimensions of an image
 print(ResizedImage.shape)
 
 cv2.imshow("Output", img)
 cv2.imshow("Resized Output", ResizedImage)
+cv2.imshow("Cropped Output", CroppedImage)
 cv2.waitKey(0)
